ff1c2c32f00d9228fc478372ce835f5d20740daf
Elias Armando <armand.g8@gmail.com>
2023-11-24 19:44:23 -0600

    156. UpperCAse, LowerCase y TitleCase Pipes

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'armando';
+  public nameUpper: string = 'ARMANDO';
+  public fullName: string = 'ElÍaS aRmAnDo';
+
 }
