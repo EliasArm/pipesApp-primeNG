91e837e9484b819c1f359fdafbf78e165edb588a
Elias Armando <armand.g8@gmail.com>
2023-11-28 17:48:31 -0600

    172. Valores y argumentos personalizados al Pipe

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'products-order',
@@ -8,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
 
+  public items: MenuItem[] = [];
+
+ngOnInit() {
+    this.items = [
+        {
+            label: 'Update',
+            icon: 'pi pi-refresh'
+        },
+        {
+            label: 'Delete',
+            icon: 'pi pi-times'
+        },
+        {
+            label: 'Angular',
+            icon: 'pi pi-external-link',
+            url: 'http://angular.io'
+        },
+        {
+            label: 'Router',
+            icon: 'pi pi-upload',
+            routerLink: '/fileupload'
+        }
+    ];
+}
 }
